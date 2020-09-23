55fc19a9a5f620780249b9792d8e69e9ec5a3cce
Bryan Paget <bryan.paget@hc-sc.gc.ca>
2020-09-23 00:04:55 -0400

    feat: lightbulbs

--- functions.R
+++ functions.R
@@ -8,9 +8,9 @@ inactivity <- "
       window.onclick     = resetTimer;
       window.onscroll    = resetTimer;
       window.onkeypress  = resetTimer;
-    
+
     function logout() { window.close(); }
-    
+
     function resetTimer() {
       clearTimeout(t);
       t = setTimeout(logout, 120000);  // milliseconds
@@ -20,24 +20,24 @@ inactivity <- "
 
 
 budget_plot <- function(ds) {
-  
+
   ds$total <- ds$Capital + ds$Non_Capital
-  
+
   ds <- ds %>%
     mutate(
       label = case_when(
-        
-        `Authority vs. Expenditures` == 'Project Expenditures' 
+
+        `Authority vs. Expenditures` == 'Project Expenditures'
         ~ paste0('Expenditure: $', prettyNum(Non_Capital, big.mark = ',')),
-        
-        `Authority vs. Expenditures` == 'Project Authority' & Capital != 0 
+
+        `Authority vs. Expenditures` == 'Project Authority' & Capital != 0
         ~ paste0('Authority: $', prettyNum(total, big.mark = ','),
                  '\n',
                  'Non-Capital: $', prettyNum(Non_Capital, big.mark = ',')),
-        
-        `Authority vs. Expenditures` == 'Project Authority' & Capital == 0 
+
+        `Authority vs. Expenditures` == 'Project Authority' & Capital == 0
         ~ paste0('Authority: $', prettyNum(total, big.mark = ','))))
-  
+
   plot_ly(
     ds %>% filter(`Authority vs. Expenditures` == 'Project Authority'),
     x = ~ year - 0.2,
@@ -84,8 +84,8 @@ budget_plot <- function(ds) {
                         2022),
         tickmode = "array")) %>%
     config(displayModeBar = F) %>%
-    layout(showlegend = FALSE) 
-  
+    layout(showlegend = FALSE)
+
 }
 
 my_dollar <- function(x){
@@ -94,16 +94,16 @@ my_dollar <- function(x){
 }
 
 budget_plot2<-function(ds){
-  
+
   min <- ifelse(min(ds$value)<0,abs(min(ds$value))*-1.2,0)
   max <- max(ds$value)*1.2
-    
+
   ds$col<-ifelse(ds$value>=0,'#1f77b4','#980008')
-  
+
   p<-ggplot(ds,aes(x=cat,y=value,fill=col))+geom_bar(stat='identity',position='dodge')+
     scale_fill_manual(values=c('#1f77b4','#980008'))+
     guides(fill=FALSE)
-  
+
   p+scale_y_continuous(labels=my_dollar,limits=c(min,max))+
     labs(y='Budget Amount')+
     geom_text(aes(label=dollar(value),vjust=ifelse(value>0,-1,1.5)),position = position_dodge(width = 1))+
@@ -113,81 +113,81 @@ budget_plot2<-function(ds){
           legend.text=element_text(size=12,family='sans',color='#494949'),
           legend.justification = 'top')+
     coord_flip()
-  
+
 }
 
 # ========= Timeplot ----
 
 timeplot<-function(df){  # removed argument internal
-  
+
   status_levels <- c(
     "forecasted completion date is within 3 months of baseline date", # green
     "forecasted completion date within 3-6 months of baseline date", # yellow
     "forecasted completion over 6 months of baseline date", # red
     "completed" # black
   )
-  
+
   status_colors <- c( "#00B050", "#FFC000", "#C00000", "#000000")
-  
+
   df$Schedule.Health.Standard <- factor(df$Schedule.Health.Standard,
                                         levels = status_levels,
                                         ordered = TRUE)
-  
+
   positions <- c(0.4, -0.4, 0.5, -0.5,0.9,-0.9,1.2,-1.25)
-  
+
   directions <- c(1, -1)
-  
+
   line_pos <- data.frame(
     "date" = sort(unique(df$Actual_date), na.last = T),
     "position"=rep(positions, length.out=length(unique(df$Actual_date))),
     "direction"=rep(directions, length.out=length(unique(df$Actual_date)))
   )
-  
+
   df<-left_join(df,line_pos,by=c('Actual_date'='date'))
   text_offset <- 0.25
-  
+
   df$month_count <- ave(df$Actual_date==df$Actual_date, df$Actual_date, FUN=cumsum)
   df$text_position <- (df$month_count * text_offset * df$direction) + df$position
-  
+
   month_buffer <- 6 # was 4 but 6 makes the projects that don't have tasks in 2018-2019 look more centered
-  
+
   # Jodi's sanity check
-  
+
   from <- min(df$Actual_date,na.rm=TRUE) - months(month_buffer)
   to <- max(df$Actual_date,na.rm=TRUE) + months(month_buffer)
-  
+
   if (is.na(from)) {
     from <- min(df$Actual_date,na.rm=TRUE)
   }
-  
+
   if (is.na(to)) {
     to <- max(df$Actual_date,na.rm=TRUE)
   }
-  
+
   month_date_range <- seq(from, to, by='month')
   month_df <- data.frame(month_date_range)
   month_df$month_format <- paste0(year(month_df$month_date_range),' ',quarters(month_df$month_date_range))
-  
+
   month_df$month_format <- ifelse(
     month_df$month_format == lag(month_df$month_format, default=''),
     '',
     month_df$month_format)
-  
+
   timeline_plot <- ggplot(
     data = df,
     aes(x = Actual_date,
         y = 0,
         label = Major.Milestone,
         color = Schedule.Health.Standard)) +
-    
+
     scale_color_manual(values = status_colors,
                        labels = status_levels,
                        drop = FALSE) +
-    
+
     labs(col="") +
     theme_classic() +
     geom_hline(yintercept = 0, color = "black", size=0.3) +
-    
+
     # Plot vertical segment lines for milestones
     geom_segment(data=df[df$month_count == 1,], aes(y=position,yend=0,xend=Actual_date), color='black', size=0.2)+
     geom_point(aes(y=0), size=3)+  # Plot scatter points at zero and date
@@ -196,7 +196,7 @@ timeplot<-function(df){  # removed argument internal
     theme(
       legend.text=element_text(size=8,color='#494949')
     )
-    
+
   # Don't show axes, appropriately position legend
   timeline_plot<-timeline_plot+
     theme(axis.line.y=element_blank(),
@@ -208,7 +208,7 @@ timeplot<-function(df){  # removed argument internal
           axis.ticks.x =element_blank(),
           axis.line.x =element_blank(),
           legend.position="bottom")
-  
+
   return(timeline_plot)
 }
 
@@ -223,17 +223,17 @@ is.sf_proj <- function(proj_name) {
 }
 
 status_plot <- function(df, x_axis_label) {
- 
+
   y_max         <- max(df[["Approved Budget"]])
   y_upper_limit <- y_max + 0.1 * y_max #' add 10% whitespace to top.
 
-  #' Colors from: https://adminlte.io/themes/AdminLTE/pages/UI/general.html 
+  #' Colors from: https://adminlte.io/themes/AdminLTE/pages/UI/general.html
   colors <- c("On-Track" = "#00a65a",
               "Caution"  = "#f39c12",
               "Delayed"  = "#f56954")
-  
+
   label = ""
-  
+
   df %>%
     arrange(status) %>%
     ggplot(
@@ -267,14 +267,14 @@ status_plot <- function(df, x_axis_label) {
 }
 
 stage_plot <- function(df) {
-   
+
   colors <- c("On-Track"        = "#00a65a",
               "Caution"         = "#f39c12",
               "Delayed"         = "#f56954",
               "Not yet started" = "#3c8dbc")
-  
+
   label <- ""
-  
+
   ggplot(
     data = df,
     aes(x = stage, y = count, group = status, fill = status)) +
@@ -291,14 +291,62 @@ stage_plot <- function(df) {
   #legend.justification = 'top',
   #legend.text=element_text(size=12,color='#494949'),
   #legend.spacing = unit(1.0,'cm'))
-  
+
 }
 
-colorfulDashboardBadge <- function(text, ...) {
+
+colorfulDashboardBadge <- function(record, ...) {
   #' @todo: add logic for choosing color based on project health or something
   #' useful like that.
-  #' @todo: instead of text, accept a row of data and in the row we have cells 
+  #' @todo: instead of text, accept a row of data and in the row we have cells
   #' with information pertaining to project status. This status information
   #' will be used to color the badge and maybe add a sub-badge?!
-  dashboardBadge(text, ...)
+  #'
+  #' Valid colors are:
+  #'
+  #'     red, yellow, aqua, blue, light-blue, green, navy, teal,
+  #'     olive, lime, orange, fuchsia, purple, maroon, black.
+  #'
+  #' Records have the following fields:
+  #'
+  #'    "IP"                      "Project"              "Current Stage"
+  #'    "Directorate"             "Project Objectives"   "Current Project Status"
+  #'    "Overall Project Health"  "status"               "stage"
+
+
+  ifelse(equals(typeof(record), typeof("c")), T, return("Not Character"))
+  ifelse(equals(nrow(record), 0), T, return("Input is Empty"))
+  ifelse(equals(dim(record), 0), T, return("Input is Empty!"))
+
+  project_name <- record["Project"][[1]]
+
+  print(project_name)
+
+  color_choices <- function(status) {
+    .color_choices = list(
+      "On-Track" = "green",
+      "Caution"  = "yellow",
+      "Delayed"  = "red",
+      "Green"    = "green",
+      "Yellow"   = "yellow",
+      "Red"      = "red")
+    return(.color_choices[status][[1]])
+  }
+
+  status <- record["status"][[1]]
+  health <- record["Overall Project Health"][[1]]
+
+  status_badge <- dashboardBadge(status, color = color_choices(status))
+  #' health_badge <- dashboardBadge(health, color = color_choices(health))
+
+  #'  tagList(project_name,
+  #'          status_badge),
+  #'          #' health_badge),
+  #'
+  #'
+  print(color_choices(health))
+
+  dashboardBadge(
+    project_name,
+    color = color_choices(health), ...)
 }
--- ui.R
+++ ui.R
@@ -7,9 +7,9 @@ library(timevis)
 library(webshot)
 library(scales)
 
-#' @todo: capitalists want to cut up your life and fight over who gets to own 
+#' @todo: capitalists want to cut up your life and fight over who gets to own
 #' the pieces.
-#' 
+#'
 #' Valid statuses are: primary, success, info, warning, danger, navy, teal, purple, orange, maroon, black.
 
 left_menu <- tagList(
@@ -17,26 +17,26 @@ left_menu <- tagList(
     id    = "download-menu",
     title = "Download",
     icon  = icon("download"),
-    
+
     downloadButton(
       outputId = "download_data",
       label    = "Data",
       icon     = icon("download")
     ),
-    
+
     conditionalPanel(
       condition = "input.sidebar == 'individual' ",
-      
+
       downloadButton(
         outputId = "downloadreport_individual",
         label    = "Report",
         icon     = icon("download")
       )
     ),
-    
+
     conditionalPanel(
       condition = "input.sidebar == 'overview' ",
-      
+
       downloadButton(
         outputId = "downloadreport_overview",
         label    = "Report",
@@ -44,7 +44,7 @@ left_menu <- tagList(
       )
     )
   ),
-  
+
   actionButton(
     inputId = "contact",
     label   = "Contact",
@@ -65,25 +65,25 @@ header <- dashboardHeaderPlus(
 
 sidebar_menu <- sidebarMenu(
   id = "sidebar",
-  
+
   menuItem(
     text    = "Overview",
     tabName = "overview",
     icon    = icon("globe-africa")
   ),
-  
+
   menuItem(
     text    = "Individual",
     tabName = "individual",
     icon    = icon("search")
   ),
-  
+
   menuItem(
     text    = "About",
     tabName = "explanations",
     icon    = icon("info")
   )
-  
+
 )
 
 
@@ -93,7 +93,7 @@ sidebar <- dashboardSidebar(sidebar_menu)
 rightsidebar <- rightSidebar(
   title = "Right Sidebar",
   background = "dark",
-  
+
   rightSidebarTabContent(
     id     = 1,
     title  = "Tab 1",
@@ -107,7 +107,7 @@ rightsidebar <- rightSidebar(
       value   = 500
     )
   ),
-  
+
   rightSidebarTabContent(
     id    = 2,
     title = "Tab 2",
@@ -115,7 +115,7 @@ rightsidebar <- rightSidebar(
               label = "Caption",
               "Data Summary")
   ),
-  
+
   rightSidebarTabContent(
     id    = 3,
     icon  = "paint-brush",
@@ -134,242 +134,191 @@ rightsidebar <- rightSidebar(
 body <- dashboardBody(tabItems(
   tabItem(
     tabName = "overview",
-  
-    fluidRow(   
+
+    fluidRow(
     column(
       width = 12,
-      
-      br(),
-      
-        boxProfile(
-          title = "TBI Projects",
-          subtitle = tagList(paste("Last updated:", data_date)),
-          src = "https://icons.iconarchive.com/icons/graphicloads/medical-health/128/heart-beat-icon.png",
-         
-      br(),
-      
+
       boxPlus(
         width = 12,
           solidHeader = T,
-          status = "success",
+          status = "primary",
           background    = NULL,
           boxToolSize   = "md",
           closable      = F,
           collapsible   = T,
-          title = "Project Status & Stages",
-          footer = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          title = "Progress",
           fluidRow(
               #' @todo: code should be repeated call to a function, not so much
               #' copy and pasting...
               #' @todo: group by project type, IP, IT, Innovation
               #' #todo: want to have different tabs?
-            br(),
+            box(
+              width = 3,
+              title = "Innovation Projects",
+              status = "primary",
+              solidHeader = T,
             box(
               title = "Stage 1",
-              width = 3,
-              background = "light-blue",
+              width = 12,
+              status = "primary",
               #' @todo: please make the color reflect project health?!
-              #' I have started the function colorfulDashboardBadge, needs 
+              #' I have started the function colorfulDashboardBadge, needs
               #' implementation.
               tagList(
-                lapply(
-                  X = as.list(stage_1$IP),
+                apply(
+                  X = stage_1,
+                  MARGIN = 1,
                   FUN = function(x) (colorfulDashboardBadge(x))))
             ),
             box(
               title = "Stage 2",
-              width = 3,
-              background = "purple",
+              width = 12,
+              status = "primary",
               tagList(
-                lapply(
-                  X = as.list(stage_2$IP),
+                apply(
+                  X = stage_2,
+                  MARGIN = 1,
                   FUN = function(x) (colorfulDashboardBadge(x))))
             ),
             box(
               title = "Stage 3",
-              width = 3,
-              background = "blue",
+              width = 12,
+              status = "primary",
               tagList(
-                lapply(
-                  X = as.list(stage_3$IP),
+                apply(
+                  X = stage_3,
+                  MARGIN = 1,
                   FUN = function(x) (colorfulDashboardBadge(x))))
             ),
             box(
               title = "Stage 4",
-              width = 3,
-              background = "navy",
+              status = "primary",
+              width = 12,
               tagList(
-                lapply(
-                  X = as.list(stage_4$IP), 
-                  FUN = function(x) (colorfulDashboardBadge(x, color = "blue"))))
-            ),
+                apply(
+                  X = stage_4,
+                  MARGIN = 1,
+                  FUN = function(x) (colorfulDashboardBadge(x))))
+            )),
+            box(
+              width = 3,
+              status = "primary",
+              title = "IP Projects",
+              solidHeader = T,
             box(
               title = "On Track",
-              width = 3,
-              background = "green",
+              width = 12,
               tagList(
-                lapply(
-                  X = as.list(on_track$IP),
-                  FUN = function(x) (colorfulDashboardBadge(x, color = "yellow"))))
+                apply(
+                  X = on_track,
+                  MARGIN = 1,
+                  FUN = function(x) (colorfulDashboardBadge(x))))
             ),
             box(
-              width = 3,
+              width = 12,
               title = "Caution",
-              background = "orange",
               tagList(
-                lapply(
-                  X = as.list(caution$IP),
+                apply(
+                  X = caution,
+                  MARGIN = 1,
                   FUN = function(x) (colorfulDashboardBadge(x))))
             ),
             box(
               title = "Delayed",
-              width = 3,
-              background = "red",
+              width = 12,
               tagList(
-                lapply(
-                  X = as.list(delayed$IP),
+                apply(
+                  X = delayed,
+                  MARGIN = 1,
                   FUN = function(x) (colorfulDashboardBadge(x))))
-            ),
+            )
+            )
+            ,
+            box(
+              width = 3,
+              title = "IP Projects",
+              status = "primary",
+              solidHeader = T,
               box(
               title = "Planning",
-              width = 3,
-              background = "black",
+              width = 12,
               tagList(
-                lapply(
-                  X = as.list(planning$IP),
+                apply(
+                  X = planning,
+                  MARGIN = 1,
                   FUN = function(x) (colorfulDashboardBadge(x))))
             ),
               box(
               title = "Testing",
-              width = 3,
-              background = "black",
+              width = 12,
               tagList(
-                lapply(
-                  X = as.list(testing$IP), 
+                apply(
+                  X = testing,
+                  MARGIN = 1,
                   FUN = function(x) (colorfulDashboardBadge(x))))
-            ),
-            box(
-              title = "Completed",
-              width = 3,
-              background = "black",
-              tagList(
-                lapply(
-                  X = as.list(schedule_completed_project_names$IP),
-                  FUN = function(x) (colorfulDashboardBadge(x))))
-            ),
-          
-          
+            )),
+
           )),
-    )),
-    
+    ),
+
     column(
       width = 12,
-      br(),
-      tags$h2("Project Health"),
-      br(),
-     
-      
-     tabBox(
-       width = 12,
-        tabPanel(
-          title = "Overview",
-          fluidRow(
-        box(
-          title = "Green Health",
-          background = "green",
-          tagList(as.list(green$IP))
-        ),
-         box(
-          title = "Yellow Health",
-          background = "yellow",
-          tagList(as.list(yellow$IP))
-        ),
-         box(
-          title = "Red Health",
-          background = "red",
-          tagList(as.list(red$IP))
-        ))),
-        tabPanel(
-          title = "Innovation Projects",
-          fluidRow(
-          box(
-            width = 12,
+
+      box(
+        width = 12,
           collapsible = T,
+          closable = F,
+        title = "Balance",
+          status = "success",
+          solidHeader = T,
+          box(
+            width = 4,
+          collapsible = F,
           closable = F,
           title = "Innovation Projects",
           solidHeader = T,
           status = "success",
-          "Can I write something here?",
-          footer = tagList(
-          dashboardLabel("On Track", status = "success"),
-          dashboardLabel("Caution", status = "warning"),
-          dashboardLabel("Delayed", status = "danger")
-        ),
-          withSpinner(plotlyOutput("innovation_projects_health"))
-        )
-        )),
-        
-        tabPanel(
-          title = "A Team",
-         
-          fluidRow(  
+         withSpinner(plotlyOutput("innovation_projects_health"))),
+
         box(
-            width = 12,
-          collapsible = T,
+            width = 4,
+          collapsible = F,
           closable = F,
           title = "A Team Projects",
           solidHeader = T,
           status = "success",
-          "Can I write something here?",
-          footer = tagList(
-          dashboardLabel("On Track", status = "success"),
-          dashboardLabel("Caution", status = "warning"),
-          dashboardLabel("Delayed", status = "danger")
-        ),
-          withSpinner(plotlyOutput("a_team_projects_health"))
-        )
-        
-        )),
-        
-        tabPanel(
-          title = "IP Projects",
-          fluidRow(
+          withSpinner(plotlyOutput("a_team_projects_health"))),
+
            box(
-            width = 12,
-          collapsible = T,
+            width = 4,
+          collapsible = F,
           closable = F,
           title = "IP Projects",
           status = "success",
           solidHeader = T,
-          "Can I write something here?",
-          footer = tagList(
+
+          withSpinner(plotlyOutput("ip_projects_health"))),
+        footer = tagList(
           dashboardLabel("On Track", status = "success"),
           dashboardLabel("Caution", status = "warning"),
           dashboardLabel("Delayed", status = "danger")
-        ),
-          withSpinner(plotlyOutput("ip_projects_health"))
         )
-        
-        )
+
         ),
-      uiOutput("overall_project_health"))),
-   
-    br(), 
+
+      uiOutput("overall_project_health")),
+
     column(
-      width = 12, 
-    br(), 
-      tags$h2("Project Budget"),
-    br()
-    
-    ),
-    br(), 
-    
-    column(width = 12, uiOutput("project_portfolio_budget")),
-    
+      width = 12,
+    boxPlus(
+      width = 12,
+      uiOutput("project_portfolio_budget"))),
+
     column(
       width = 12,
       tags$h1("Scheduling"),
-      br(), 
+      br(),
       boxPlus(
         status = "navy",
         enable_dropdown = T,
@@ -394,7 +343,7 @@ body <- dashboardBody(tabItems(
       )
     )
     #' @todo: need to remove logout button, these BR are a work-aroud.
-   
+
   ),
    br(),
     br(),
@@ -405,22 +354,22 @@ body <- dashboardBody(tabItems(
     br(),
     br(),
     br()
-    
+
   ),
-  
-  
+
+
   tabItem(
     tabName = "individual",
-    
+
     column(
       width = 12,
-      
+
       selectInput(
         inputId = "selectip",
         label   = "Select an IP project",
         choices = ip
       ),
-      
+
       widgetUserBox(
         title = textOutput("project_name"),
         subtitle = "Something useful",
@@ -429,9 +378,6 @@ body <- dashboardBody(tabItems(
         src = "https://upload.wikimedia.org/wikipedia/commons/d/d8/Crystal_Project_Package_games_board.png",
         boxToolSize = "xs",
         width = 12,
-        tags$p(
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        ),
         footer = tagList(
           valueBoxOutput("overall"),
           valueBoxOutput("overall_stage"),
@@ -439,18 +385,18 @@ body <- dashboardBody(tabItems(
         )
       )
     ),
-    
+
     fluidRow(column(
       width = 12,
            withSpinner(plotlyOutput("budget_plt")),
-      
+
       boxPlus(
         title = "Projections",
         footer = "Something.",
         withSpinner(plotOutput("budget_all"))),
-      
+
     )),
-    
+
     fluidRow(column(
       width = 12,
       box(
@@ -459,7 +405,7 @@ body <- dashboardBody(tabItems(
         dataTableOutput("proj_risk_tb")
       )
     )),
-    
+
     fluidRow(column(
       width = 12,
       box(
@@ -468,7 +414,7 @@ body <- dashboardBody(tabItems(
         dataTableOutput("proj_issue_tb")
       )
     )),
-    
+
     fluidRow(column(
       width = 12,
       box(title = "Schedule",
@@ -477,15 +423,15 @@ body <- dashboardBody(tabItems(
           ))
     ))
   ),
-  
+
   tabItem(
     tabName = "explanations",
-    
+
     column(width = 12,
            fluidRow(uiOutput(
              "explanations_header"
            ))),
-    
+
     column(width = 12,
            fluidRow(includeHTML(
              "explanations.html"
@@ -496,7 +442,7 @@ body <- dashboardBody(tabItems(
 
 ui <- secure_app(
   head_auth = tags$script(inactivity),
-  
+
   dashboardPagePlus(
     collapse_sidebar = T,
     header       = header,
