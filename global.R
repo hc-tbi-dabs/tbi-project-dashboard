e7dfe4be308519b7f05eafeba63b58e23e9841e4
Bryan Paget <bryan.paget@hc-sc.gc.ca>
2020-09-23 00:04:55 -0400

    feat: lightbulbs

--- global.R
+++ global.R
@@ -22,7 +22,6 @@ library(magrittr)
 library(DT)
 library(ggrepel)
 library(devtools)
-# library(shinyauthr)
 library(shinyjs)
 library(lubridate)
 library(orca)
@@ -31,80 +30,73 @@ library(rsconnect)
 library(webshot)
 library(timevis)
 
+source('functions.R')
+
+
 credentials <- data.frame(
-  user = c("dabs"),
-  password = c("TBI"),
-  # comment = c("alsace", "auvergne", "bretagne"), %>% 
-  stringsAsFactors = FALSE
+  user             = c("dabs"),
+  password         = c("TBI"),
+  stringsAsFactors = F
 )
 
-# all raw data from dattbi.xlsx ----
-source('functions.R')
-description <- read_excel('dattbi.xlsx',1)
-schedule <- read_excel('dattbi.xlsx',2)
-budget <- read_excel('dattbi.xlsx',3)
-budget_yr <- read_excel('dattbi.xlsx',4)
-status <- read_excel('dattbi.xlsx',5) # all_proj in old
-all_proj <- read_excel('dattbi.xlsx',5)
-proj_risk <- read_excel('dattbi.xlsx',6)
-proj_issue <- read_excel('dattbi.xlsx',7)
-capital <- read_excel('dattbi.xlsx',8)
 
-# removing IPA01 from descriptions, this is DABs and have no schedule, budget, risk or issues
-# won't show in individual
+description <- read_excel('dattbi.xlsx', 1)
+schedule    <- read_excel('dattbi.xlsx', 2)
+budget      <- read_excel('dattbi.xlsx', 3)
+budget_yr   <- read_excel('dattbi.xlsx', 4)
+status      <- read_excel('dattbi.xlsx', 5) #' all_proj in old
+all_proj    <- read_excel('dattbi.xlsx', 5)
+proj_risk   <- read_excel('dattbi.xlsx', 6)
+proj_issue  <- read_excel('dattbi.xlsx', 7)
+capital     <- read_excel('dattbi.xlsx', 8)
+
+
+#' removing IPA01 from descriptions, this is DABs and have no schedule, 
+#' budget, risk or issues won't show in individual
 description <- description %>%
-  filter(!(description$IP=="A01"))
+  filter(!(description$IP == "A01"))
 
-# date of which data is updated ----
+
 data_date <- substring(file.info("dattbi.xlsx")$mtime, 1, 11)
 
 
-# data cleaning removed all rows with any NA observations----
-# systematic
-nona_description <- description %>%
-  drop_na()
-nona_schedule <- schedule %>%
-  drop_na()
-nona_budget <- budget %>%
-  drop_na()
-nona_budget_yr <- budget_yr %>%
-  drop_na()
-nona_status <- status %>%
-  drop_na()
-nona_proj_risk <- proj_risk %>%
-  drop_na()
-nona_proj_issue <- proj_issue %>%
-  drop_na()
-nona_capital <- capital %>%
-  drop_na()
+#' Data cleaning:
+nona_description <- description %>% drop_na()
+nona_schedule    <- schedule    %>% drop_na()
+nona_budget      <- budget      %>% drop_na()
+nona_budget_yr   <- budget_yr   %>% drop_na()
+nona_status      <- status      %>% drop_na()
+nona_proj_risk   <- proj_risk   %>% drop_na()
+nona_proj_issue  <- proj_issue  %>% drop_na()
+nona_capital     <- capital     %>% drop_na()
 
-# formatting dates ----
-schedule$Actual_date<-as.Date(as.character(schedule$Actual_date))
-schedule$Approved_finish_date<-as.Date(as.character(schedule$Approved_finish_date))
-proj_issue$`Target Date for Resolution`<-as.Date(as.character(proj_issue$`Target Date for Resolution`))
+#' Formatting dates:
+schedule$Actual_date                    <- as.Date(as.character(schedule$Actual_date))
+schedule$Approved_finish_date           <- as.Date(as.character(schedule$Approved_finish_date))
+proj_issue$`Target Date for Resolution` <- as.Date(as.character(proj_issue$`Target Date for Resolution`))
 
-# pasting schedule IP to major milestone blurb ----
-schedule$Major.Milestone<-paste0(schedule$IP,":",schedule$Major.Milestone)
-schedule$Major.Milestone.Standard<-paste0(schedule$Project,":",schedule$Major.Milestone.Standard)
+#' Pasting schedule IP to major milestone blurb:
+schedule$Major.Milestone <- paste0(schedule$IP, ":", schedule$Major.Milestone)
+schedule$Major.Milestone.Standard <- paste0(schedule$Project, ":", schedule$Major.Milestone.Standard)
 
-# list of ip and directorate ----
-ip<-c(description$IP[!is.na(description$IP)])
-directorate<-c('All') # Martin wants it to only say all
+#' List of ip and directorate:
+ip          <- c(description$IP[!is.na(description$IP)])
+directorate <- c("All") #' Martin wants it to only say all
 
-# status cleaning ----  
-status$`Overall Project Health`[is.na(status$`Overall Project Health`)]<-'Blue'
-status$status[is.na(status$status)]<-'Not yet started'
+#' Status cleaning:
+status$`Overall Project Health`[is.na(status$`Overall Project Health`)] <- "Blue"
+status$status[is.na(status$status)]                                     <- "Not yet started"
 
-# capital ----
-capital$IP <- as.character(capital$IP)
-capital$`Authority vs. Expenditures`<-'Project Authority'
+#' Capital:
+capital$IP                           <- as.character(capital$IP)
+capital$`Authority vs. Expenditures` <- "Project Authority"
 
-# data budget ----
-budget_yr%<>%left_join(capital)
-budget_yr$Value<-ifelse(is.na(budget_yr$Value),0,budget_yr$Value)
-budget_yr$Capital<-ifelse(is.na(budget_yr$Capital),0,budget_yr$Capital)
-budget_yr$Non_Capital<-budget_yr$Value-budget_yr$Capital
-budget_yr$year<-as.numeric(substr(budget_yr$Year,1,4))
+#' Data budget:
+budget_yr             %<>% left_join(capital)
+budget_yr$Value       <-   ifelse(is.na(budget_yr$Value), 0, budget_yr$Value)
+budget_yr$Capital     <-   ifelse(is.na(budget_yr$Capital), 0, budget_yr$Capital)
+budget_yr$Non_Capital <-   budget_yr$Value-budget_yr$Capital
+budget_yr$year        <-   as.numeric(substr(budget_yr$Year, 1, 4))
 
 # sorted budget ----
 # for reporting purposes
